ea4bd710fc8cb8b01764a6e506d208faf7d358af
Rampravesh25 <rampraveshmallah@gmail.com>
2022-05-24 22:12:56 +0530

    This is Master

---
+++ src/com/snake/and/ladder/UC1SnakeAndLadder.java
@@ -0,0 +1,13 @@
+package com.snake.and.ladder;
+
+import java.util.Random;
+
+public class UC1SnakeAndLadder {
+    public static void main(String[] args) {
+        int n = 0;
+        Random ran = new Random();
+        n = ran.nextInt(7);
+        System.out.println("Print the " + n);
+    }
+}
+
