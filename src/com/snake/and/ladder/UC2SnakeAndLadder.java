5eaee7924567fcac6c1be58b16a83a65fcff9532
Rampravesh25 <rampraveshmallah@gmail.com>
2022-05-24 22:29:13 +0530

    Second UC2

---
+++ src/com/snake/and/ladder/UC2SnakeAndLadder.java
@@ -0,0 +1,13 @@
+package com.snake.and.ladder;
+
+
+public class UC2SnakeAndLadder {
+    public static void main(String[] args) {
+        System.out.println("Welcome to this Snake And Ladder");
+
+        int i = 0;
+        for (i = 0; i <= 100; i++) {
+            System.out.println(" " + i);
+        }
+    }
+}
