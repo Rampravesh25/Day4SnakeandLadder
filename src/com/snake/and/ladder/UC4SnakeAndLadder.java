b10a2a0a390c2ccbb2b264169f8c6bed2da374d2
Rampravesh25 <rampraveshmallah@gmail.com>
2022-05-24 23:33:47 +0530

    Forth UC3

---
+++ src/com/snake/and/ladder/UC4SnakeAndLadder.java
@@ -0,0 +1,19 @@
+package com.snake.and.ladder;
+
+import java.util.Random;
+
+public class UC4SnakeAndLadder {
+    public static void main(String[] args) {
+
+        int zeroPosition = 0;
+        int ladder = 2;
+        int snake = 3;
+        
+        Random ran = new Random();
+
+        int diceValue = 1 + ran.nextInt(6);
+
+
+    }
+
+}
