8e0e5b8c4dec2f9cdfdfa7dba1fd3e36a0b81700
Rampravesh25 <rampraveshmallah@gmail.com>
2022-05-25 18:54:01 +0530

    Forth UC3

--- src/com/snake/and/ladder/UC4SnakeAndLadder.java
+++ src/com/snake/and/ladder/UC4SnakeAndLadder.java
@@ -6,14 +6,23 @@ public class UC4SnakeAndLadder {
     public static void main(String[] args) {
 
         int zeroPosition = 0;
-        int ladder = 2;
+     /*   int ladder = 2;
         int snake = 3;
-        
+*/
         Random ran = new Random();
 
         int diceValue = 1 + ran.nextInt(6);
 
+        System.out.println("this +" + diceValue);
+
+
+        while (diceValue < 101) {
+            if (diceValue == 100) {
+                System.out.println("Dice is reached :- "+diceValue);
+
+            }
+            diceValue++;
+        }
 
     }
-
 }
